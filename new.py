c6d3e8d63d818ce477359a90d050369ae62c4d93
davidhabboosh1 <habboosh.d@northeastern.edu>
2025-02-05 10:57:58 -0500

    get rid of false leads

--- new.py
+++ new.py
@@ -6,8 +6,8 @@ from scipy.linalg import solve_discrete_are
 model = mujoco.MjModel.from_xml_path("scene.xml")
 data = mujoco.MjData(model)
 
-for _ in range(100):
-    mujoco.mj_step(model, data)
+# for _ in range(100):
+#     mujoco.mj_step(model, data)
 
 ctrl0 = data.ctrl.copy()
 
@@ -16,17 +16,24 @@ nu = model.nu
 
 R = np.eye(nu)
 
+qpos0 = data.qpos.copy()
+
+count = 0
+
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
-        qpos0 = data.qpos.copy()
-        ctrl0 = data.ctrl.copy()
+        count += 1
+        
+        # data.qpos = qpos0
+        # qpos0 = data.qpos.copy()
+        # ctrl0 = data.ctrl.copy()
         
         A = np.zeros((2 * nv, 2 * nv))
         B = np.zeros((2 * nv, nu))
         mujoco.mjd_transitionFD(model, data, 1e-6, True, A, B, None, None)
         
-        mujoco.mj_resetData(model, data)
-        data.qpos = qpos0
+        # mujoco.mj_resetData(model, data)
+        # data.qpos = qpos0
         mujoco.mj_forward(model, data)
         
         jac_com = np.zeros((3, nv))
@@ -61,7 +68,7 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         balance_dofs = body_dofs
         other_dofs = np.setdiff1d(body_dofs, balance_dofs)
         
-        BALANCE_COST        = 1000
+        BALANCE_COST        = 10000
         BALANCE_JOINT_COST  = 3
         OTHER_JOINT_COST    = .3
 
@@ -75,9 +82,9 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         Q = np.block([[Qpos, np.zeros((nv, nv))],
                     [np.zeros((nv, 2*nv))]])
 
-        mujoco.mj_resetData(model, data)
-        data.ctrl = ctrl0
-        data.qpos = qpos0
+        # mujoco.mj_resetData(model, data)
+        # data.ctrl = ctrl0
+        # data.qpos = qpos0
         
         A = np.zeros((2 * nv, 2 * nv))
         B = np.zeros((2 * nv, nu))
@@ -85,11 +92,17 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         flg_centered = True
         mujoco.mjd_transitionFD(model, data, epsilon, flg_centered, A, B, None, None)
         
-        P = solve_discrete_are(A, B, Q, R)
+        try:
+            P = solve_discrete_are(A, B, Q, R)
+        except np.linalg.LinAlgError as e:
+            print(count)
+            mujoco.mj_step(model, data)
+            continue
+            
         K = np.linalg.inv(R + B.T @ P @ B) @ B.T @ P @ A
         
-        mujoco.mj_resetData(model, data)
-        data.qpos = qpos0
+        # mujoco.mj_resetData(model, data)
+        # data.qpos = qpos0
         
         dq = np.zeros(nv)
         
